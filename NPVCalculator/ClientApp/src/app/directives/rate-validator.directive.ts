e4752d70d1119abc3e00d7b7087761266fc697c5
Jacel Enova <jacel.enova@yahoo.com>
2019-04-17 19:16:17 +0800

    Fix rate validator not updating ng-valid property.

--- NPVCalculator/ClientApp/src/app/directives/rate-validator.directive.ts
+++ NPVCalculator/ClientApp/src/app/directives/rate-validator.directive.ts
@@ -1,5 +1,5 @@
-import { Directive, Input } from '@angular/core';
-import { NG_VALIDATORS, AbstractControl, ValidationErrors } from '@angular/forms';
+import { Directive, Input, ViewChild, ElementRef } from '@angular/core';
+import { NG_VALIDATORS, AbstractControl, ValidationErrors, FormControl, NgModel, NgControl, NgForm } from '@angular/forms';
 
 @Directive({
     selector: '[appRateValidator]',
@@ -7,15 +7,35 @@ import { NG_VALIDATORS, AbstractControl, ValidationErrors } from '@angular/forms
 })
 export class RateValidatorDirective {
     // tslint:disable-next-line:no-input-rename
-    @Input('appRateValidator') rateValidator: void;
-    @Input() low: number;
-    @Input() high: number;
+    @Input('appRateValidator') form: NgForm;
+    @Input() low: string;
+    @Input() high: string;
 
     constructor() { }
 
-    validate(control: AbstractControl): ValidationErrors {
-        if (this.low > this.high) {
-            return { rateValidation: 'Rate validation failed.' };
+    validate(): ValidationErrors {
+        if (this.form) {
+            const lowControl = this.form.controls[this.low];
+            const upperControl = this.form.controls[this.high];
+
+            if (lowControl && upperControl) {
+                if (lowControl.value > upperControl.value) {
+                    console.log('error');
+                    lowControl.setErrors({ rateValidation: 'Rate validation failed.' });
+                    lowControl.markAsTouched();
+                    upperControl.setErrors({ rateValidation: 'Rate validation failed.' });
+                    upperControl.markAsTouched();
+                } else {
+                    if (lowControl.hasError('rateValidation')) {
+                        lowControl.setErrors({'rateValidation': null});
+                        lowControl.updateValueAndValidity();
+                    }
+                    if (upperControl.hasError('rateValidation')) {
+                        upperControl.setErrors({'rateValidation': null});
+                        upperControl.updateValueAndValidity();
+                    }
+                }
+            }
         }
         return null;
     }
