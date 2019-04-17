781378b5385b6f77ff0e4338bbaa630fea7dc9a6
Jacel Enova <jacel.enova@yahoo.com>
2019-04-17 16:50:59 +0800

    Fetch previous npv requests.

--- NPVCalculator/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ NPVCalculator/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,23 +1,19 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NPVQuery } from '../classes/npvquery';
+import { NPVService } from '../services/npv.service';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
-  selector: 'app-fetch-data',
-  templateUrl: './fetch-data.component.html'
+    selector: 'app-fetch-data',
+    templateUrl: './fetch-data.component.html'
 })
-export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+export class FetchDataComponent implements OnInit  {
+    public queries: Observable<Array<NPVQuery>>;
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
-  }
-}
+    constructor(private npvService: NPVService, @Inject('BASE_URL') baseUrl: string, http: HttpClient) { }
 
-interface WeatherForecast {
-  dateFormatted: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+    ngOnInit() {
+        this.queries = this.npvService.getPreviousQueries();
+    }
 }
